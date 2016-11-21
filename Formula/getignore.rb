82ac8a8dbdd98406360d0f7d15cbc6a045203a20
Chris Lasher <chris.lasher@gmail.com>
2016-11-20 16:39:16 -0800

    Initial commit

---
+++ Formula/getignore.rb
@@ -0,0 +1,29 @@
+class Getignore < Formula
+  desc "A command-line tool to create gitignore files from a repository of gitignore patterns"
+  homepage "https://github.com/gotgenes/getignore"
+  url "https://github.com/gotgenes/getignore/archive/0.1.0.tar.gz"
+  sha256 "ed877423f3e4e1dff61a0c6837bca6ae2eda6c296a17343ba101f58446abbda1"
+
+  head "https://github.com/gotgenes/getignore.git"
+
+  depends_on "go" => :build
+  depends_on "glide" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
+
+    getignore_path = buildpath/"src/github.com/gotgenes/getignore"
+    getignore_path.install buildpath.children
+
+    cd getignore_path do
+      system "glide", "install"
+      system "go", "build", "-o", "getignore"
+      bin.install "getignore"
+    end
+  end
+
+  test do
+    system "getignore" "--version"
+  end
+end
