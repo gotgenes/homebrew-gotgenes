259afa6f2088d149cd56586a8c52bcdb1ba568d0
Chris Lasher <chris.lasher@gmail.com>
2021-09-05 16:19:01 -0700

    Update to getignore 2.1.1.
    Fix build to include LDFLAGS via make.

--- Formula/getignore.rb
+++ Formula/getignore.rb
@@ -3,15 +3,16 @@ class Getignore < Formula
   homepage "https://github.com/gotgenes/getignore"
   url "https://github.com/gotgenes/getignore.git",
       {
-        tag:      "v2.1.0",
-        revision: "549da0a0f7d4151cb288efceea4947e9c6bc8250",
+        tag:      "v2.1.1",
+        revision: "53759eca2d334f414878a79cda7bd6e162d818bd",
       }
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    system "make"
+    bin.install "#{name}"
     bash_completion.install "completions/bash/getignore-completion.bash"
     zsh_completion.install "completions/zsh/_getignore"
   end
